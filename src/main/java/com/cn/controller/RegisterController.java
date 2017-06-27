3e092373458421b43240bcd63d09ce98d0036816
Administrator <Administrator@SCBWMF01>
2017-06-27 11:14:36 +0800

    生成dao mapper pojo

---
+++ src/main/java/com/cn/controller/RegisterController.java
@@ -0,0 +1,20 @@
+/**   
+* @Title: Register.java 
+* @Package com.cn.controller 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author （作者）徐伟  
+* @date 2017年6月27日 上午11:00:03 
+* @version V1.0   
+*/
+package com.cn.controller;
+
+/** 
+* @ClassName: Register 
+* @Description: 注册 
+* @author (作者) 徐伟 
+* @date 2017年6月27日 上午11:00:03 
+* @version V1.0 
+*/
+public class RegisterController {
+
+}
